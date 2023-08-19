8479c5f54fc8e1878220216d1e722289e6bc5a87
91779 <91779@LAPTOP-JB011T0O>
2023-08-19 22:20:24 +0530

    sa

--- practice/src/interviewPractice/ArrayMaxOrMin.java
+++ practice/src/interviewPractice/ArrayMaxOrMin.java
@@ -3,7 +3,7 @@ package interviewPractice;
 public class ArrayMaxOrMin {
 
 	public static void main(String[] args) {
-		int n[]= {20,30,60,40,90,103,0403,50,51,53,54};
+		int n[]= {20,30,60,40,90,103,0403,50,51,53,54,57};
 	    int max=n[0];
 		for(int i=1;i<n.length;i++)
 		{
