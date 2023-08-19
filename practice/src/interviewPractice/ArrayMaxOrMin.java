6e005eec23c1b8dc12225eec9186e69bb63ad193
91779 <91779@LAPTOP-JB011T0O>
2023-08-19 19:50:26 +0530

    sc

--- practice/src/interviewPractice/ArrayMaxOrMin.java
+++ practice/src/interviewPractice/ArrayMaxOrMin.java
@@ -3,7 +3,6 @@ package interviewPractice;
 public class ArrayMaxOrMin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int n[]= {20,30,60,40,80};
 	    int max=n[0];
 		for(int i=1;i<n.length;i++)
